ead847d8eef319adfd0ace178f20ce718ebe8737
jfuellem <jonasfuellemann10@bluewin.ch>
2023-11-12 13:06:20 +0100

    Implementing musicmanager

--- MissingGame/Assets/Scripts/MusicManager.cs
+++ MissingGame/Assets/Scripts/MusicManager.cs
@@ -1,8 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
-    
+    public static MusicManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
 }
